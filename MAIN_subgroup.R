32246e322a155854189f58a1f8b03619bc76a310
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-06 16:10:51 +0800

    Update MAIN_subgroup.R

--- MAIN_subgroup.R
+++ MAIN_subgroup.R
@@ -1,1 +1,8 @@
+library(survival)
+library(survminer)
 
+##TIME-INDEPENDENT
+##LST AND MD BY AGE
+model1<-coxph(Surv(md_time,md)~blst*relevel(",j,", ref='",k,"')+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)
+model2<-coxph(Surv(",y,",",x,")~",z,"*relevel(",j,", ref='",l,"')+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)
+ 
