c77d5c23d1424e6e6ff9b446c47bfa2f7b2f266b
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:46:13 +0800

    Update WM_liner.R

--- WM_liner.R
+++ WM_liner.R
@@ -1,7 +1,10 @@
 ##blst = baseline summer normalized LST as continious exposure
 ##buhii = baseline summer normalized UHII as continious exposure
-##wm_md_tfma = tract forceps major
-##the secondary outcome included 30 WM microstructure as follows: c
+##wm_md_tfma = tract forceps major for md value
+##the secondary outcome included 15 WM microstructure for MD and ISOVF values as follows: tract forceps major; tract forceps minor; tract middle cerebellar peduncle;
+##tract acoustic radiation; tract anterior thalamic radiation; tract cingulate gyrus part of cingulum; tract corticospinal tract; 
+##tract inferior fronto-occipital fasciculus; tract inferior longitudinal fasciculus; tract medial lemniscus; tract parahippocampal part of cingulum;
+##tract posterior thalamic radiation; tract superior longitudinal fasciculus; tract superior thalamic radiation; tract uncinate fasciculus 
 
 
 ###LST AND WM
