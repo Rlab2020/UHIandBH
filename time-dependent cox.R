01c7494060bdba356dc6e2aefa541a97011c4157
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:07:30 +0800

    Update time-dependent cox.R

--- time-dependent cox.R
+++ time-dependent cox.R
@@ -1,9 +1,10 @@
-##md_time = follow-up period
-##md = mental disorder (1 or 0)
-##blst = baseline summer normalized LST as continious exposure
-##blstc = baseline summer normalized LST as categorial exposure
-##buhii = baseline summer normalized UHII as continious exposure
-##buhii = baseline summer normalized UHII as categorial exposure
+##start = start from each year
+##stop = end of each year
+##event = mental disorder (1 or 0)
+##lst = summer normalized LST as continious exposure
+##lstc = summer normalized LST as categorial exposure
+##uhii = summer normalized UHII as continious exposure
+##uhiic = summer normalized UHII as categorial exposure
 
 
 library(survival)
@@ -23,6 +24,8 @@ model5 <- coxph(Surv(start, stop, event)~uhii, data_md)
 model6 <- coxph(Surv(start, stop, event)~uhiic, data_md)
 
 ##UHII AND MD MULTIVARIABLE MODEL
-model7 <- coxph(Surv(start, stop, event)~uhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud)
-model8 <- coxph(Surv(start, stop, event)~uhiic+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud) 
+model7 <- coxph(Surv(start, stop, event)~uhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md)
+model8 <- coxph(Surv(start, stop, event)~uhiic+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md) 
 
+
+The associations of UHI and substance use disorder, depressive disorder, and anxiety disorder were also obtained by running the above code, respectively.
