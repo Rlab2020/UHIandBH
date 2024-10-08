1758df6b36fdc270941f30475f750db7360a5e09
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:01:52 +0800

    Update time-independent cox.R

--- time-independent cox.R
+++ time-independent cox.R
@@ -10,24 +10,24 @@ library(survival)
 library(survminer)
 
 ##LST AND MD UNIVARIABLE MODEL
-model1 <- coxph(Surv(md_time,md)~blst, dataset) ##continious exposure
-model2 <- coxph(Surv(md_time,md)~blstc, dataset) ##categorial exposure
+model1 <- coxph(Surv(md_time,md)~blst, dataset)
+model2 <- coxph(Surv(md_time,md)~blstc, dataset) 
 
 
 ##LST AND MD MULTIVARIABLE MODEL
-model3 <- coxph(Surv(md_time,md)~blst+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)  ##continious exposure
-model4 <- coxph(Surv(md_time,md)~blstc+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset) ##categorial exposure
+model3 <- coxph(Surv(md_time,md)~blst+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset) 
+model4 <- coxph(Surv(md_time,md)~blstc+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)
 
 
 
 ##UHII AND MD UNIVARIABLE MODEL
-model5 <- coxph(Surv(md_time,md)~buhii, dataset) ##continious exposure
-model6 <- coxph(Surv(md_time,md)~buhiic, dataset) ##categorial exposure
+model5 <- coxph(Surv(md_time,md)~buhii, dataset)
+model6 <- coxph(Surv(md_time,md)~buhiic, dataset) 
 
 
 ##UHII AND MD MULTIVARIABLE MODEL
-model7 <- coxph(Surv(md_time,md)~buhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)  ##continious exposure
-model8 <- coxph(Surv(md_time,md)~buhiic+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset) ##categorial exposure
+model7 <- coxph(Surv(md_time,md)~buhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)  
+model8 <- coxph(Surv(md_time,md)~buhiic+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,dataset)
 
 
 The associations of UHI and substance use disorder, depressive disorder, and anxiety disorder were also obtained by running the above code, respectively.
