baae36b285f1d7d01f8edcc97430dc6b4abffbe6
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:48:43 +0800

    Update sem.R

--- sem.R
+++ sem.R
@@ -1,49 +1,34 @@
+
+
+
 library(lavaan)
 
-####md model 
+####md model for LST
 mymodel <- '  ### mesurement model
               happy =~ fhapp + fhh + flife
               phq9 =~  finad + ftired + fdepr + fconc + fappe + finte
               gad7 =~  fwor + frelax + fann + fworr + ffore + fnerv
               ### regressions moodel
-              md ~   buhiis + happy + phq9 + gad7
-              happy ~ phq9 + gad7 + buhiis
-              gad7 ~ buhiis 
-              phq9 ~ buhiis '
+              md ~   blst + happy + phq9 + gad7
+              happy ~ phq9 + gad7 + blst
+              gad7 ~ blst 
+              phq9 ~ blst '
 fit <- sem(mymodel, data=newdata1, ordered = c("md"), std.lv=T)
 summary(fit, fit.measures=TRUE)
 
-####sud model
+
+
+####md model for UHII
 mymodel <- '  ### mesurement model
               happy =~ fhapp + fhh + flife
               phq9 =~  finad + ftired + fdepr + fconc + fappe + finte
               gad7 =~  fwor + frelax + fann + fworr + ffore + fnerv
               ### regressions moodel
-              sud ~   buhiis + happy + phq9 + gad7
-              happy ~ phq9 + gad7 + buhiis
-              gad7 ~ buhiis 
-              phq9 ~ buhiis '
-fit <- sem(mymodel, data=newdata1, ordered = c("sud"), std.lv=T)
+              md ~   buhii + happy + phq9 + gad7
+              happy ~ phq9 + gad7 + buhii
+              gad7 ~ buhii 
+              phq9 ~ buhii '
+fit <- sem(mymodel, data=newdata1, ordered = c("md"), std.lv=T)
 summary(fit, fit.measures=TRUE)
 
-####mdd model
-mymodel <- '  ### mesurement model
-              happy =~ fhapp + fhh + flife
-              phq9 =~  finad + ftired + fdepr + fconc + fappe + finte
-              ### regressions moodel
-              mdd ~   buhiis + happy + phq9 
-              happy ~ phq9 + buhiis
-              phq9 ~ buhiis '
-fit <- sem(mymodel, data=newdata1, ordered = c("mdd"), std.lv=T)
-summary(fit, fit.measures=TRUE)
 
-####gad model
-mymodel <- '  ### mesurement model
-              happy =~ fhapp + fhh + flife
-              gad7 =~  fwor + frelax + fann + fworr + ffore + fnerv
-              ### regressions moodel
-              gad ~ buhiis + happy +  gad7
-              happy ~ buhiis + gad7
-              gad7 ~ buhiis '
-fit <- sem(mymodel, data=newdata1, ordered = c("gad"), std.lv=T)
-summary(fit, fit.measures=TRUE)
