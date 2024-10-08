37d809718a8eac5b74ce7eaaa7d15acab4e85e9a
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:01:11 +0800

    Update time-dependent cox.R

--- time-dependent cox.R
+++ time-dependent cox.R
@@ -1,99 +1,28 @@
+##md_time = follow-up period
+##md = mental disorder (1 or 0)
+##blst = baseline summer normalized LST as continious exposure
+##blstc = baseline summer normalized LST as categorial exposure
+##buhii = baseline summer normalized UHII as continious exposure
+##buhii = baseline summer normalized UHII as categorial exposure
+
+
 library(survival)
 library(survminer)
 
 ##LST AND MD UNIVARIABLE MODEL
-model1 <- coxph(Surv(start, stop, event)~lst, data_md) ##continious exposure
-summary(model1)
-model2 <- coxph(Surv(start, stop, event)~lstc, data_md) ##categorial exposure
-summary(model2)
+model1 <- coxph(Surv(start, stop, event)~lst, data_md)
+model2 <- coxph(Surv(start, stop, event)~lstc, data_md) 
 
 ##LST AND MD MULTIVARIABLE MODEL
-model3 <- coxph(Surv(start, stop, event)~lst+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md)  ##continious exposure
-summary(model3)
-model4 <- coxph(Surv(start, stop, event)~lstc+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md) ##categorial exposure
-summary(model4)
-
-##LST AND SUD UNIVARIABLE MODEL
-model5 <- coxph(Surv(start, stop, event)~lst, data_sud) ##continious exposure
-summary(model5)
-model6 <- coxph(Surv(start, stop, event)~lstc, data_sud) ##categorial exposure
-summary(model6)
-
-##LST AND SUD MULTIVARIABLE MODEL
-model7 <- coxph(Surv(start, stop, event)~lst+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud)  ##continious exposure
-summary(model7)
-model8 <- coxph(Surv(start, stop, event)~lstc+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud) ##categorial exposure
-summary(model8)
-
-##LST AND MDD UNIVARIABLE MODEL
-model9 <- coxph(Surv(start, stop, event)~lst, data_mdd) ##continious exposure
-summary(model9)
-model10 <- coxph(Surv(start, stop, event)~lstc, data_mdd) ##categorial exposure
-summary(model10)
-
-##LST AND MDD MULTIVARIABLE MODEL
-model11 <- coxph(Surv(start, stop, event)~lst+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_mdd)  ##continious exposure
-summary(model11)
-model12 <- coxph(Surv(start, stop, event)~lstc+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_mdd) ##categorial exposure
-summary(model12)
-
-##LST AND GAD UNIVARIABLE MODEL
-model13 <- coxph(Surv(start, stop, event)~lst, data_gad) ##continious exposure
-summary(model13)
-model14 <- coxph(Surv(start, stop, event)~lstc, data_gad) ##categorial exposure
-summary(model14)
-
-##LST AND GAD MULTIVARIABLE MODEL
-model15 <- coxph(Surv(start, stop, event)~lst+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_gad)  ##continious exposure
-summary(model15)
-model16 <- coxph(Surv(start, stop, event)~lstc+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_gad) ##categorial exposure
-summary(model16)
+model3 <- coxph(Surv(start, stop, event)~lst+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md) 
+model4 <- coxph(Surv(start, stop, event)~lstc+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_md) 
 
 
 ##UHII AND MD UNIVARIABLE MODEL
-model1 <- coxph(Surv(start, stop, event)~uhii, data_md) ##continious exposure
-summary(model1)
-model2 <- coxph(Surv(start, stop, event)~uhiic, data_md) ##categorial exposure
-summary(model2)
+model5 <- coxph(Surv(start, stop, event)~uhii, data_md)
+model6 <- coxph(Surv(start, stop, event)~uhiic, data_md)
 
 ##UHII AND MD MULTIVARIABLE MODEL
-model3 <- coxph(Surv(start, stop, event)~uhii+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud)  ##continious exposure
-summary(model3)
-model4 <- coxph(Surv(start, stop, event)~uhiic+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud) ##categorial exposure
-summary(model4)
+model7 <- coxph(Surv(start, stop, event)~uhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud)
+model8 <- coxph(Surv(start, stop, event)~uhiic+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud) 
 
-##UHII AND SUD UNIVARIABLE MODEL
-model5 <- coxph(Surv(start, stop, event)~uhii, data_sud) ##continious exposure
-summary(model5)
-model6 <- coxph(Surv(start, stop, event)~uhiic, data_sud) ##categorial exposure
-summary(model6)
-
-##UHII AND SUD MULTIVARIABLE MODEL
-model7 <- coxph(Surv(start, stop, event)~uhii+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud)  ##continious exposure
-summary(model7)
-model8 <- coxph(Surv(start, stop, event)~uhiic+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_sud) ##categorial exposure
-summary(model8)
-
-##UHII AND MDD UNIVARIABLE MODEL
-model9 <- coxph(Surv(start, stop, event)~uhii, data_mdd) ##continious exposure
-summary(model9)
-model10 <- coxph(Surv(start, stop, event)~uhiic, data_mdd) ##categorial exposure
-summary(model10)
-
-##UHII AND MDD MULTIVARIABLE MODEL
-model11 <- coxph(Surv(start, stop, event)~uhii+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_mdd)  ##continious exposure
-summary(model11)
-model12 <- coxph(Surv(start, stop, event)~uhiic+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_mdd) ##categorial exposure
-summary(model12)
-
-##UHII AND GAD UNIVARIABLE MODEL
-model13 <- coxph(Surv(start, stop, event)~uhii, data_gad) ##continious exposure
-summary(model13)
-model14 <- coxph(Surv(start, stop, event)~uhiic, data_gad) ##categorial exposure
-summary(model14)
-
-##UHII AND GAD MULTIVARIABLE MODEL
-model15 <- coxph(Surv(start, stop, event)~uhii+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_gad)  ##continious exposure
-summary(model15)
-model16 <- coxph(Surv(start, stop, event)~uhiic+age0+sex+edu+income+ethnic+whr0+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp,data_gad) ##categorial exposure
-summary(model16)
