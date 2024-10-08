e09a1102a6035514b5f05427b44213aff68e2ea5
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:42:00 +0800

    Update SYMPTOM_logistic.R

--- SYMPTOM_logistic.R
+++ SYMPTOM_logistic.R
@@ -1,7 +1,7 @@
 ##blst = baseline summer normalized LST as continious exposure
 ##buhii = baseline summer normalized UHII as continious exposure
 ##fhapp = General happy (1/0)
-##the secondary outcome included 19 psychiatric symptoms as follows：General happy; Happiness with own health; Belief that own life is meaningful;
+##the secondary outcome included 19 psychiatric symptoms as follows: General happy; Happiness with own health; Belief that own life is meaningful;
 ##Recent thoughts of suicide or self-harm; Trouble falling asleep, or sleeping too much; Recent changes in speed of moving or speaking; Recent feelings of inadequacy;
 ##Recent feelings of tiredness or low energy; Recent feelings of depression; Recent trouble concentrating on things; Recent poor appetite or overeating;
 ##Recent lack of interest or pleasure in doing things; Recent inability to stop or control worrying; Recent restlessness; Recent trouble relaxing; 
