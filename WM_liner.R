3d401d84ce6181b0e24c3a8cec9c981ae06bbc0d
Rlab2020 <107493985+Rlab2020@users.noreply.github.com>
2024-10-08 10:40:48 +0800

    Update WM_liner.R

--- WM_liner.R
+++ WM_liner.R
@@ -1,9 +1,13 @@
+##blst = baseline summer normalized LST as continious exposure
+##buhii = baseline summer normalized UHII as continious exposure
+##wm_md_tfma = tract forceps major
+##
 
 
 ###LST AND WM
-model1 <- glm(wm_md_tfma~blsts+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp, family='gaussian', dataset)
+model1 <- glm(wm_md_tfma~blst+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp, family='gaussian', dataset)
 
 ###UHI AND WM
-model2 <- glm(wm_md_tfma~buhiis+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp, family='gaussian', dataset)
+model2 <- glm(wm_md_tfma~buhii+age+sex+edu+income+ethnic+whr+nosmoking+drinkingM+sleeph+regular+dieth+htnp+dmp+cadp, family='gaussian', dataset)
 
 ##The associations of UHI and other WM microstructure were also obtained by running the above code, respectively.
